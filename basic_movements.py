b2e5f91043de463976aeb06ad4d11b032a4e585e
Shreyan Kumar Mishra <79162142+Shreyan34@users.noreply.github.com>
2025-09-16 21:56:46 +0530

    Update basic_movements.py to include comments

--- basic_movements.py
+++ basic_movements.py
@@ -1,45 +1,54 @@
-import pygame
+import pygame # import dependencies
 
+# initialize pygame
 pygame.init()
+
+# declare constants
 WIDTH, HEIGHT = 600, 600
 X = WIDTH / 2
 Y = HEIGHT / 2
+
+# define x and y speed
 x_speed = 0.05
 y_speed = 0.05
 RADIUS = 20
 
+# create pygame window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Basic Key Movements")
+pygame.display.set_caption("Basic Key Movements") # set the title of the window
 
+# draws a circle on the screen
 def drawCircle():
     pygame.draw.circle(screen, [255, 255, 255], [X, Y], RADIUS)
 
 def keyMovements():
+    # a function which performs key movements
     global X
     global Y
-
+    
+    # get the keys
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a]: # if key is a, then move forward
         X -= x_speed
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d]: # if key is d, then move backward
         X += x_speed
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w]: # if key is w, then move up
         Y -= y_speed
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s]: # if key is s, then move down
         Y += y_speed
 
-
+# create game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    keyMovements()
-    drawCircle()
+    keyMovements() # call the function to perform key movements
+    drawCircle() # draw the circle
 
-    pygame.display.update()
+    pygame.display.update() # update the screen
 
-    screen.fill([0, 0, 0])
+    screen.fill([0, 0, 0]) # fill the screen
 
-pygame.quit()
+pygame.quit() # close the game, after completion of program
